bbe90ba370f208b9c96ba31fac0a215badc71fe6
marchese89 <marchese89@hotmail.com>
2023-11-09 17:41:47 +0100

    .

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -25,9 +25,9 @@ export default function ArticlesList(){
         <Container>
             <Row className="justify-content-start d-flex gy-4">
         {
-            articles.map((article,i)=>{
+            articles.map((article)=>{
             return(
-            <Col className="justify-content-center d-flex" md={4} xs={12} key={i}>
+            <Col className="justify-content-center d-flex" md={4} xs={12} key={article.id}>
               <ArticleComponent article={article} clickable={true}/>
               </Col>)
             })
